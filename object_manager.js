f2b6f34e454eb3c0b8fe40279d69e66e4e536a3a
imasha85 <imasha@yandex.ru>
2017-09-22 11:32:56 +0300

    Add files via upload

---
+++ object_manager.js
@@ -0,0 +1,28 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [41.884654, 12.569449],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            clusterize: true,
+            // ObjectManager принимает те же опции, что и кластеризатор.
+            gridSize: 150,
+            clusterIconLayout: "default#pieChart"
+        });
+
+    // Чтобы задать опции одиночным объектам и кластерам,
+    // обратимся к дочерним коллекциям ObjectManager.
+    myMap.geoObjects.add(objectManager);
+
+    $.ajax({
+        url: "data.json"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
+
+}
